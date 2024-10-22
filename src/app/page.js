99f1d1bbbd3edd69473ec8d14be89742ba216043
Shashwat Bagaria <shashwawtforwork@gmail.com>
2024-10-23 00:32:41 +0530

    Update copy and theme for the website

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { BsYoutube } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -7,89 +11,64 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          style={{ alignSelf: "center" }}
+          src="/thebrightpause-logo.svg"
+          alt="The Bright Pause logo"
           width={180}
-          height={38}
+          height={180}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
+        <section className={styles.body}>
+          <p>
+            Today's social media landscape pushes you to compare your life to
+            others, whether you realize it or not. LinkedIn makes you question
+            your career, Twitter leaves you feeling dumb, and Instagram makes it
+            seem like you're not enjoying life enough. The overwhelming amount
+            of negative news, ads, and videos leaves you feeling drained.
+          </p>
+          <br />
+          <p>
+            The Bright Pause is the bringing in a change by reminding people
+            that reality isn’t as bad as it often seems. It aims to give you a
+            break from your routine virtual scroll, offering optimism, and a
+            moment of mindful awareness for your real life.
+          </p>
+        </section>
+        <section className={styles.socialLinkContainer}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://instagram.com/thebrightpause"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <BsInstagram />
+            Instagram
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://x.com/thebrightpause"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.secondary}
           >
-            Read our docs
+            <BsTwitterX />
+            Twitter
           </a>
-        </div>
+          <a
+            href="https://www.linkedin.com/in/thebrightpause"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsLinkedin />
+            LinkedIn
+          </a>
+          <a
+            href="https://youtube.com/@thebrightpause"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsYoutube />
+            YouTube
+          </a>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
