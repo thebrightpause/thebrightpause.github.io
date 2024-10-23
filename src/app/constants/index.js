f74dce5b19602c767f856c30c78d0f4b8f724b09
Shashwat Bagaria <shashwawtforwork@gmail.com>
2024-10-23 21:52:33 +0530

    Make Responsive and add meta tags

---
+++ src/app/constants/index.js
@@ -0,0 +1,7 @@
+export const THE_BRIGHT_PAUSE = {
+  title: "The Bright Pause",
+  image:
+    "https://raw.githubusercontent.com/thebrightpause/thebrightpause.github.io/refs/heads/main/thebrightpause-wall.jpg",
+  description: "Serving up small bites of joy to snack on while you scroll.",
+  url: "https://www.thebrightpause.com",
+};
--- src/app/page.js
+++ src/app/page.js
@@ -18,57 +18,59 @@ export default function Home() {
           height={180}
           priority
         />
-        <section className={styles.body}>
-          <p>
-            Today&apos;s social media landscape pushes you to compare your life
-            to others, whether you realize it or not. LinkedIn makes you
-            question your career, Twitter leaves you feeling dumb, and Instagram
-            makes it seem like you are not enjoying life enough. The
-            overwhelming amount of negative news, ads, and videos leaves you
-            feeling drained.
-          </p>
-          <br />
-          <p>
-            The Bright Pause is the bringing in a change by reminding people
-            that reality isn&apos;t as bad as it often seems. It aims to give
-            you a break from your routine virtual scroll, offering optimism, and
-            a moment of mindful awareness for your real life.
-          </p>
-        </section>
-        <section className={styles.socialLinkContainer}>
-          <a
-            href="https://instagram.com/thebrightpause"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsInstagram />
-            Instagram
-          </a>
-          <a
-            href="https://x.com/thebrightpause"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsTwitterX />
-            Twitter
-          </a>
-          <a
-            href="https://www.linkedin.com/in/thebrightpause"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsLinkedin />
-            LinkedIn
-          </a>
-          <a
-            href="https://youtube.com/@thebrightpause"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsYoutube />
-            YouTube
-          </a>
-        </section>
+        <div className={styles.contentContainer}>
+          <section className={styles.body}>
+            <p>
+              Today&apos;s social media landscape pushes you to compare your
+              life to others, whether you realize it or not. LinkedIn makes you
+              question your career, Twitter leaves you feeling dumb, and
+              Instagram makes it seem like you are not enjoying life enough. The
+              overwhelming amount of negative news, ads, and videos leaves you
+              feeling drained.
+            </p>
+            <br />
+            <p>
+              The Bright Pause is the bringing in a change by reminding people
+              that reality isn&apos;t as bad as it often seems. It aims to give
+              you a break from your routine virtual scroll, offering optimism,
+              and a moment of mindful awareness for your real life.
+            </p>
+          </section>
+          <section className={styles.socialLinkContainer}>
+            <a
+              href="https://instagram.com/thebrightpause"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsInstagram />
+              Instagram
+            </a>
+            <a
+              href="https://x.com/thebrightpause"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsTwitterX />
+              Twitter
+            </a>
+            <a
+              href="https://www.linkedin.com/in/thebrightpause"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsLinkedin />
+              LinkedIn
+            </a>
+            <a
+              href="https://youtube.com/@thebrightpause"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsYoutube />
+              YouTube
+            </a>
+          </section>
+        </div>
       </main>
     </div>
   );
