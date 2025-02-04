27426bdb9adeb2d135cc9c4349a54a840313212d
Shashwat Bagaria <shashwawtforwork@gmail.com>
2025-02-04 17:47:28 +0530

    remove cloud

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,36 +5,37 @@ import { BsLinkedin } from "react-icons/bs";
 import styles from "../page.module.css";
 import IconButton from "./IconButton";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<div className="flex flex-wrap items-center gap-2">
-			<Image
-				className={styles.logo}
-				style={{ alignSelf: "center" }}
-				src="/thebrightpause-logo.svg"
-				alt="The Bright Pause logo"
-				width={180}
-				height={180}
-				priority
-			/>
-			<div className="hidden sm:block h-8 border-l border-white mr-8"></div>
-			<div className="flex items-center text-sm lg:text-xl font-bright-pause">
-				Find more&nbsp;
-				{/* <span className="hidden lg:block">bright pauses&nbsp;</span> */}
-				<span>bright pauses&nbsp;</span>
-				at
-			</div>
-			{/* <IconButton Icon={BsYoutube} link="https://youtube.com/@thebrightpause" /> */}
-			<IconButton
-				Icon={BsInstagram}
-				link="https://instagram.com/thebrightpause"
-			/>
-			<IconButton Icon={BsTwitterX} link="https://twitter.com/thebrightpause" />
-			<IconButton
-				Icon={BsLinkedin}
-				link="https://linkedin.com/company/thebrightpause"
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-wrap items-center gap-2">
+      <Link href={"/"}>
+        <Image
+          className={styles.logo}
+          style={{ alignSelf: "center" }}
+          src="/thebrightpause-logo.svg"
+          alt="The Bright Pause logo"
+          width={180}
+          height={180}
+          priority
+        />
+      </Link>
+      <div className="hidden sm:block h-8 border-l border-white mr-8"></div>
+      <div className="flex items-center text-sm lg:text-xl font-bright-pause">
+        Find more&nbsp;
+        <span>bright pauses&nbsp;</span>
+        at
+      </div>
+      <IconButton
+        Icon={BsInstagram}
+        link="https://instagram.com/thebrightpause"
+      />
+      <IconButton Icon={BsTwitterX} link="https://twitter.com/thebrightpause" />
+      <IconButton
+        Icon={BsLinkedin}
+        link="https://linkedin.com/company/thebrightpause"
+      />
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,47 +4,50 @@ import styles from "./page.module.css";
 import Footer from "./components/Footer";
 
 export default function Home() {
-	return (
-		<div className={styles.page}>
-			<main className={styles.main}>
-				<Image
-					className={styles.logoDesktop}
-					style={{ alignSelf: "center" }}
-					src="/thebrightpause-logo.svg"
-					alt="The Bright Pause logo"
-					width={180}
-					height={180}
-					priority
-				/>
-				<div>
-					<div className={`${styles.breatheEntry} text-sm lg:text-xl`}>
-						Feeling Drained? Take a quick&nbsp;
-						<Link href="/breathe" className={styles.cloud}></Link> with us.
-					</div>
-					<div className={styles.contentDivider}></div>
-				</div>
-				<div className={styles.contentContainer}>
-					<section className={`${styles.body} text-sm lg:text-xl`}>
-						<p>
-							Today&apos;s social media landscape pushes you to compare your
-							life to others, whether you realize it or not. LinkedIn makes you
-							question your career, Twitter leaves you feeling dumb, and
-							Instagram makes life seem less exciting. The overwhelming amount
-							of negative news, ads, and videos leaves you feeling drained.
-						</p>
-						<br />
-						<p>
-							The Bright Pause is bringing in a change by reminding people that
-							reality isn&apos;t as bad as it often seems. It offers a break
-							from routine scrolling, with moments of optimism and mindful
-							awareness for real life.
-						</p>
-					</section>
-				</div>
-				<div className="fixed bottom-0 left-0 right-0 flex justify-center pb-8">
-					<Footer />
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logoDesktop}
+          style={{ alignSelf: "center" }}
+          src="/thebrightpause-logo.svg"
+          alt="The Bright Pause logo"
+          width={180}
+          height={180}
+          priority
+        />
+        <div>
+          <div className={`${styles.breatheEntry} text-sm lg:text-xl`}>
+            Feeling Drained? Take a quick&nbsp;
+            <Link href="/breathe" className="underline hover:no-underline">
+              break
+            </Link>
+            &nbsp;with us.
+          </div>
+          <div className={styles.contentDivider}></div>
+        </div>
+        <div className={styles.contentContainer}>
+          <section className={`${styles.body} text-sm lg:text-xl`}>
+            <p>
+              Today&apos;s social media landscape pushes you to compare your
+              life to others, whether you realize it or not. LinkedIn makes you
+              question your career, Twitter leaves you feeling dumb, and
+              Instagram makes life seem less exciting. The overwhelming amount
+              of negative news, ads, and videos leaves you feeling drained.
+            </p>
+            <br />
+            <p>
+              The Bright Pause is bringing in a change by reminding people that
+              reality isn&apos;t as bad as it often seems. It offers a break
+              from routine scrolling, with moments of optimism and mindful
+              awareness for real life.
+            </p>
+          </section>
+        </div>
+        <div className="fixed bottom-0 left-0 right-0 flex justify-center pb-8">
+          <Footer />
+        </div>
+      </main>
+    </div>
+  );
 }
